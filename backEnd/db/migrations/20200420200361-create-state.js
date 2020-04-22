589362d97ec2696a3f0e78fe6f93e2cfb0408c43
Seamus Le <seamust.le@gmail.com>
2020-04-21 17:16:18 -0700

    added back createdAt to state migration

--- backEnd/db/migrations/20200420200361-create-state.js
+++ backEnd/db/migrations/20200420200361-create-state.js
@@ -12,6 +12,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING(2)
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
