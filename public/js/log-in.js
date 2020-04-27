56b74ff360ab6617a1310d622dab0c0e3c35f693
Zachary Rizer <zacharyrizer@gmail.com>
2020-04-27 15:54:17 -0700

    add demo user login to register page

--- public/js/log-in.js
+++ public/js/log-in.js
@@ -1,37 +1,8 @@
 import { handleErrors, api } from "./utils.js";
+import { handleLogin } from "./loginHelperFunction.js"
 
 const logInFormUser = document.querySelector(".user-login");
 const logInFormShelter = document.querySelector(".shelter-login");
-
-export const handleLogin = async (body, authorization) => {
-    try {
-        const res = await fetch(`${api}${authorization}s/token`, {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (!res.ok) {
-            throw res;
-        }
-        const {
-            token,
-            role,
-            user: { id },
-            name,
-        } = await res.json();
-        // storage access_token in localStorage:
-        localStorage.setItem("PAWS_AND_CLAWS_ACCESS_TOKEN", token);
-        localStorage.setItem("PAWS_AND_CLAWS_CURRENT_USER_ID", id);
-        localStorage.setItem("PAWS_AND_CLAWS_ROLE", role);
-        localStorage.setItem("PAWS_AND_CLAWS_NAME", name);
-
-        window.location.href = `/${authorization}-profile`;
-    } catch (err) {
-        handleErrors(err);
-    }
-}
 
 logInFormUser.addEventListener("submit", (e) => {
     e.preventDefault();
